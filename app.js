1eb5bf22525773e1afdc2831bb2c48f8ab475d6c
Maxim Sitowskyi <sitowskyimaxim@gmail.com>
2024-06-20 01:38:23 +0300

    first version

---
+++ app.js
@@ -0,0 +1,10 @@
+function generatePassword() {
+    const length = 12; // Choosing length of the password
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&()_+~`|}{[]:;?><,./-=";
+    let password = "";
+    for (let i = 0; i < length; ++i) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    document.getElementById("password").textContent = password;
+}
