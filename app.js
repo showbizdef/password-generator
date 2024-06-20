974b255d9ae116d0c03187c22fcba4065a7d54d3
Maxim Sitowskyi <sitowskyimaxim@gmail.com>
2024-06-20 19:16:14 +0300

    update + fix
    fix:
    window with author's social now opening in center of your screen
    new:
    add some information button about passwords :)

--- app.js
+++ app.js
@@ -124,3 +124,17 @@ function openLinkWindow() {
          //   </html>
    // `);
 // }
+
+document.getElementById('open-modal-btn').onclick = function() {
+    document.getElementById('myModal').style.display = 'block';
+};
+
+document.getElementsByClassName('close')[0].onclick = function() {
+    document.getElementById('myModal').style.display = 'none';
+};
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('myModal')) {
+        document.getElementById('myModal').style.display = 'none';
+    }
+};
