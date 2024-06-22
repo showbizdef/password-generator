cd8b8e0edf8f8c1c5d343d781065a6740699cd5b
Maxim Sitowskyi <sitowskyimaxim@gmail.com>
2024-06-22 13:30:25 +0300

    update
    - the copy password button is temporarily not working
    - added a special slider to adjust the number of password characters at the user's request

--- app.js
+++ app.js
@@ -1,57 +1,47 @@
-function generatePassword() {
-    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
-    let password = '';
-    for (let i = 0; i < 12; i++) { 
-        const randomIndex = Math.floor(Math.random() * charset.length);
-        password += charset[randomIndex];
-    }
-    document.getElementById('password').value = password;
-}
 
-function copyPassword() {
+    // <button id="generate-btn" onClick={generatePassword()}>Generate</button>
+    const generateBtn = document.getElementById('generate-btn');
+    const copyBtn = document.getElementById('copy-btn');
+    const passwordLengthSlider = document.getElementById('passwordLength');
+    const lengthValueSpan = document.getElementById('lengthValue');
     const passwordField = document.getElementById('password');
-    
-    if (passwordField) {
-        passwordField.focus();
-        passwordField.select();
-        passwordField.setSelectionRange(0, 99999); 
-    
-        if (navigator.clipboard && window.isSecureContext) {
-            
-            navigator.clipboard.writeText(passwordField.value)
-                .then(() => {
-                    alert('Password copied to clipboard');
-                })
-                .catch(err => {
-                    console.error('Failed to copy: ', err);
-                });
-        } else {
-            
-            try {
-                const successful = document.execCommand('copy');
-                const msg = successful ? 'successful' : 'unsuccessful';
-                console.log('Copying text command was ' + msg);
-                alert('Password copied to clipboard');
-            } catch (err) {
-                console.error('Oops, unable to copy', err);
-            }
-        }
-    } else {
-        console.error('Element with id "password" not found or not an input element.');
-    }
-}
+    const strengthBar = document.getElementById('strength-bar');
 
-function openLinkWindow() {
-    const width = 500;
-    const height = 500;
-    const left = (screen.width / 2) - (width / 2);
-    const top = (screen.height / 2) - (height / 2);
-    const linkWindow = window.open("", "LinkWindow", `width=${width},height=${height},left=${left},top=${top}"`);
-    linkWindow.document.write(`
-        <html>
-        <head>
-            <title>Link</title>
-            <style>
+    function generatePassword() {
+        const length = passwordLengthSlider.value;
+        const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
+        let password = '';
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * charset.length);
+            password += charset[randomIndex];
+        }
+        passwordField.value = password;
+        const strength = evaluatePasswordStrength(password);
+        updateStrengthIndicator(strength);
+    }
+    
+    function copyPassword() {
+        const password = passwordField.value;
+
+        navigator.clipboard.writeText(password).then(() => {
+            console.log('Password copied to clipboard');
+            alert('Password copied to clipboard');
+        }).catch(err => {
+            console.error('Failed to copy: ', err);
+        });
+    };
+
+    function openLinkWindow() {
+        const width = 500;
+        const height = 500;
+        const left = (screen.width / 2) - (width / 2);
+        const top = (screen.height / 2) - (height / 2);
+        const linkWindow = window.open("", "LinkWindow", `width=${width},height=${height},left=${left},top=${top}"`);
+        linkWindow.document.write(`
+            <html>
+            <head>
+                <title>Link</title>
+                <style>
                 body {
                     display: flex;
                     justify-content: center;
@@ -98,43 +88,59 @@ function openLinkWindow() {
             <a href="https://www.youtube.com/channel/UCdCMvvBIMJhdPp3q_QNmz7w" id="link-button" class="button" target="_blank">Youtube</a>
         </body>
         </html>
-        `)
-}
+        `)};
 
-// function openInfoWindow() {
-   // const infoWindow = window.open("", "InfoWindow", "width=400,height=400");
-    //infoWindow.document.write(`
-      //  <html>
-        //<head>
-          //  <title>Information</title>
-            //<style>
-              //  body {
-                //font-family: "Titillium Web", sans-serif;
-                //padding: 20px;
-                //background-color: #f0f0f0;
-                //}
-                //h1 {
-                //    color: #4CAF50;
-                //}
-            //</head>
-           // <body>
-              //  <h1>Information</h1>
-              //  <p>This is some information displayed in a new window</p>
-          //  </body>
-         //   </html>
-   // `);
-// }
 
-document.getElementById('open-modal-btn').onclick = function() {
+
+
+    document.getElementById('open-modal-btn').onclick = function() {
     document.getElementById('myModal').style.display = 'block';
-};
+    };
 
-document.getElementsByClassName('close')[0].onclick = function() {
-    document.getElementById('myModal').style.display = 'none';
-};
-
-window.onclick = function(event) {
-    if (event.target == document.getElementById('myModal')) {
+    document.getElementsByClassName('close')[0].onclick = function() {
         document.getElementById('myModal').style.display = 'none';
+    };
+
+    window.onclick = function(event) {
+        if (event.target == document.getElementById('myModal')) {
+            document.getElementById('myModal').style.display = 'none';
+        }
+    };
+
+    function evaluvatePasswordStrength(password) {
+        let strength = 0;
+        if (password.length >= 8) strength++;
+        if (password.length >=12) strength++;
+        if (/[A-Z]/.test(password)) strength++;
+        if (/[a-z]/.test(password)) strength++;
+        if (/[0-9]/.test(password)) strength++;
+        if (/[^A-Za-z0-9]/.test(password)) strength++;
+
+        return strength;
     }
-};
+
+    function updateStrengthIndicator(strength) {
+        strengthBar.style.width = `${strength * 20}%`;
+
+        if (strength <2) {
+            strengthBar.style.backgroundColor = 'red';
+    }   else if (strength < 4) {
+            strengthBar.style.backgroundColor = 'orange';
+    }   else {
+        strengthBar.style.backgroundColor = 'green';
+    }
+    }
+
+    passwordLengthSlider.addEventListener('input', (event) => {
+        lengthValueSpan.textContent = event.target.value;
+    });
+
+    generateBtn.addEventListener('click', () => {
+        const passwordLength = passwordLengthSlider.value;
+        const password = generatePassword(passwordLength);
+        passwordField.value = password;
+        const strength = evaluatePasswordStrength(password);
+        updateStrengthIndicator(strength);
+
+        copyBtn.addEventListener('click', copyPassword);
+    });
